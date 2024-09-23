268ac8487d25aadd4a5b1630f70064b001b1528a
Dale Benedict Ocumen <Ocumendale11@gmail.com>
2024-09-24 01:25:30 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormPdfGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(325, 316);
+            button1.Name = "button1";
+            button1.Size = new Size(142, 38);
+            button1.TabIndex = 19;
+            button1.Text = "Generate PDF";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(283, 242);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(275, 23);
+            textBox4.TabIndex = 18;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(283, 209);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(275, 23);
+            textBox3.TabIndex = 17;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(283, 173);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(275, 23);
+            textBox2.TabIndex = 16;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(283, 135);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(275, 23);
+            textBox1.TabIndex = 15;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(174, 250);
+            label5.Name = "label5";
+            label5.Size = new Size(89, 15);
+            label5.TabIndex = 14;
+            label5.Text = "Phone number:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(174, 217);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 15);
+            label4.TabIndex = 13;
+            label4.Text = "Address:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(174, 181);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 15);
+            label3.TabIndex = 12;
+            label3.Text = "Last Name: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(174, 143);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 15);
+            label2.TabIndex = 11;
+            label2.Text = "First Name: ";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(304, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(163, 25);
+            label1.TabIndex = 10;
+            label1.Text = "Register Now!";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using MySql.Data.MySqlClient;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+
+namespace WindowsFormPdfGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create a PDF document
+            Document doc = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream("example.pdf", FileMode.Create));
+
+            // Open the document to write content
+            doc.Open();
+
+            // Add an image
+            iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance("D:\\Programs c#\\PdfGenerator\\pictures\\scene.jpg");
+            doc.Add(img);
+
+            // Add a paragraph
+            Paragraph name = new Paragraph($"Name: {textBox1.Text} {textBox2.Text}");
+            Paragraph address = new Paragraph($"Address: {textBox3.Text}");
+            Paragraph number = new Paragraph($"Phone Number: {textBox4.Text}");
+            doc.Add(name);
+            doc.Add(address);
+            doc.Add(number);
+
+            // Close the document
+            doc.Close();
+
+            MessageBox.Show("PDF GENERATED SUCCESSFULLY!");
+            GetDataFromMySQL();
+        }
+        private void GetDataFromMySQL()
+        {
+            string connectionString = "server=localhost;uid=root;pwd=dedengtangkad;database=trial_connection";
+            string query = "INSERT INTO forpdf (FirstName, LastName, Address, PhoneNumber) VALUES (@FirstName, @LastName, @Address, @PhoneNumber)";
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        // Add parameters
+                        cmd.Parameters.AddWithValue("@FirstName", textBox1.Text);
+                        cmd.Parameters.AddWithValue("@LastName", textBox2.Text);
+                        cmd.Parameters.AddWithValue("@Address", textBox3.Text);
+                        cmd.Parameters.AddWithValue("@PhoneNumber", textBox4.Text);
+
+                        // Open the connection
+                        conn.Open();
+
+                        // Execute the query
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                        // Close the connection
+                        conn.Close();
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+    }
+}
